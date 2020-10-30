5add73e593bc2399b2ac4bb589e97a8b4c212ce6
HanM.G <hanmoongoo@gmail.com>
2020-10-30 23:17:31 +0900

    antDesign, redux관련 설치, redux설정

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";  // 여러 reducer를 하나로 통합하기위해
+//import user from "./user_reducer";
+
+const  rootReducer = combineReducers({
+    //user,
+})
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd/antd.css';
+import { Provider } from 'react-redux' ;
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+
+ReactDOM.render(   
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__&&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App /> 
+  </Provider>,
   document.getElementById('root')
 );
 
